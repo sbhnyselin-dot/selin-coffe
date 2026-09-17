fdfec5dbaa354436fb4da2ed8006d34df2fab1c2
Your selin <sbhnyselin@gmail.com>
2026-09-17 12:49:53 +0330

    Update allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,10 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get('DEBUG','True') == 'True'
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["selin-coffee.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
 
 
 # ---------------------------------------------------------
